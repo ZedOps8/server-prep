7655a0184f68dee36b9114165a82a789150f5d37
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-11-01 23:07:52 +0530

    Create websocket-chat-server.js
    Real-time chat application using Socket.io for instant messaging between users

---
+++ websocket-chat-server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+
